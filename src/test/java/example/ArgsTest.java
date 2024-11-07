c26b687c340bf207936e7aadfdf34bb8c686b80b
Asher <springflybird@163.com>
2024-11-08 07:32:11 +0800

    [概要设计]:接口规格

---
+++ src/test/java/example/ArgsTest.java
@@ -0,0 +1,17 @@
+package example;
+
+import org.junit.Test;
+
+public class ArgsTest {
+    @Test
+    public void should() {
+        Options options = Args.parse(Options.class, "-l", "-p", "8080", "-d", "/use/logs");
+        // Show how to use
+        options.logging();
+        options.port();
+        options.directroy();
+    }
+
+    static record Options(@Option("-l") boolean logging, @Option("-p") int port, @Option("-d") String directory) {
+    }
+}
