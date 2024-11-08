8c11ab7dd118df3cf21fd2dbd4fbf76846d2cc6a
Asher <springflybird@163.com>
2024-11-08 22:44:40 +0800

    [Red->Green]:multi options 的情况就是元素多了一些，但是对元素对操作都是一致对，将相同对操作提取到方法中

--- src/main/java/org/args/Args.java
+++ src/main/java/org/args/Args.java
@@ -9,34 +9,40 @@ public class Args {
 
     public static <T> T parse(Class<T> optionsClass, String... args){
         try{
+            List<String> arguments = Arrays.asList(args);
             // get first constructor（eg:for boolean record, it shld be public BooleanOption(@Option("l")boolean logging){ ）
             Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
+
             // get first parameter of constructor (eg: @Option("l")boolean logging)
             Parameter parameter = constructor.getParameters()[0];
-            // get annotation of first parameter(eg: @Option("l"))
-            Option option = parameter.getAnnotation(Option.class);
 
-            List<String> arguments = Arrays.asList(args);
-
-            Object value = null;
-
-            if (parameter.getType() == boolean.class){
-                // If the flag is present, the value is true, otherwise false
-                value = arguments.contains("-" + option.value());
-            }
-            if (parameter.getType() == int.class) {
-                // First query the index of the flag ,then get the value and ensure it is an integer
-                int index = arguments.indexOf("-" + option.value());
-                value = Integer.parseInt(arguments.get(index + 1));
-            }
-            if (parameter.getType() == String.class){
-                int index = arguments.indexOf("-" + option.value());
-                value = arguments.get(index + 1);
-            }
+            Object value = parseOption(parameter, arguments);
             return (T) constructor.newInstance(value);
         }catch (Exception e){
             throw new RuntimeException(e);
         }
 
+    }
+
+    private static Object parseOption(Parameter parameter, List<String> arguments) {
+        Object value = null;
+        // get annotation of first parameter(eg: @Option("l"))
+        Option option = parameter.getAnnotation(Option.class);
+
+
+        if (parameter.getType() == boolean.class){
+            // If the flag is present, the value is true, otherwise false
+            value = arguments.contains("-" + option.value());
+        }
+        if (parameter.getType() == int.class) {
+            // First query the index of the flag ,then get the value and ensure it is an integer
+            int index = arguments.indexOf("-" + option.value());
+            value = Integer.parseInt(arguments.get(index + 1));
+        }
+        if (parameter.getType() == String.class){
+            int index = arguments.indexOf("-" + option.value());
+            value = arguments.get(index + 1);
+        }
+        return value;
     }
 }
