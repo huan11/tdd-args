ab166bad38b99125eb86d15a85ac789dd3b6766b
Asher <springflybird@163.com>
2024-11-09 15:02:32 +0800

    [Refactor] Bad Smell:Switch Statements
    Step3: Add a new inner class to implement the interface. And delegate the implement to existing method.

--- src/main/java/org/args/Args.java
+++ src/main/java/org/args/Args.java
@@ -58,4 +58,11 @@ public class Args {
         // If the flag is present, the value is true, otherwise false
         return arguments.contains("-" + option.value());
     }
+
+    static class BooleanParser implements OptionParser{
+        @Override
+        public Object parse(List<String> arguments, Option option) {
+            return parseBoolean(arguments, option);
+        }
+    }
 }
