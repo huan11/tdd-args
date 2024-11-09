f33419bcf69cb845e8351ccec4bdf6c9dda79d8b
Asher <springflybird@163.com>
2024-11-09 19:32:53 +0800

    [Refactor] Bad Smell:IntParser 和 StringParser 代码有大量的重复
    Step10: generate constructor for child and pass function in constructor.

--- src/main/java/org/args/StringParser.java
+++ src/main/java/org/args/StringParser.java
@@ -1,8 +1,13 @@
 package org.args;
 
 import java.util.List;
+import java.util.function.Function;
 
 class StringParser extends IntParser {
+    public StringParser() {
+        super(String::valueOf);
+    }
+
     @Override
     protected  String parseValue(String value) {
         return String.valueOf(value);
