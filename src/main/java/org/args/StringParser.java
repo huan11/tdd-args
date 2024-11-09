acf84753fb9be9b6520ee1cd7e9c668b1ebce6f3
Asher <springflybird@163.com>
2024-11-09 19:58:47 +0800

    [Refactor] Bad Smell:IntParser 和 StringParser 代码有大量的重复
    Step 14: 让 StringParser 尽可能使用 Interface （OptionParser）

--- src/main/java/org/args/StringParser.java
+++ src/main/java/org/args/StringParser.java
@@ -1,14 +1,11 @@
 package org.args;
 
-import java.util.List;
-import java.util.function.Function;
-
 class StringParser extends IntParser {
     private StringParser() {
         super(String::valueOf);
     }
 
-    public static StringParser createStringParser() {
+    public static OptionParser createStringParser() {
         return new StringParser();
     }
 }
