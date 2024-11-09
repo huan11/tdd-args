e23faa99bf1a6e9aaa1d461f14ae7d20377ee4fa
Asher <springflybird@163.com>
2024-11-09 16:37:55 +0800

    [Refactor] Bad Smell:Switch Statements
    Step15:目前代码还有一个分支，一眼看上去是一个 factory, 我们可以用多态去重构，但是使用多态的前提是我们可以对 class 进行修改，但是 java 基本类型我们修改不了，但是我们可以对它进行拓展，使用一个 Map， 将 If else 转成一个查找处理.

--- src/main/java/org/args/Args.java
+++ src/main/java/org/args/Args.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Args {
 
@@ -27,18 +28,13 @@ public class Args {
         return getOptionParser(parameter.getType()).parse(arguments, parameter.getAnnotation(Option.class));
     }
 
+    private static Map<Class<?>, OptionParser> PARSERS = Map.of(
+            boolean.class, new BooleanParser(),
+            int.class, new IntParser(),
+            String.class, new StringParser()
+    );
     private static OptionParser getOptionParser(Class<?> type) {
-        OptionParser parser = null;
-        if (type == boolean.class){
-            parser = new BooleanParser();
-        }
-        if (type == int.class) {
-            parser = new IntParser();
-        }
-        if (type == String.class){
-            parser = new StringParser();
-        }
-        return parser;
+        return PARSERS.get(type);
     }
 
     interface OptionParser{
