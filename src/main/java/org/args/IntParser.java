785b48f77f372abd357f66430178bb5e71996b07
Asher <springflybird@163.com>
2024-11-09 17:10:21 +0800

    [Refactor] Bad Smell:IntParser 和 StringParser 代码有大量的重复
    Step2: 准备步骤，把不一样的部分extract 到方法

--- src/main/java/org/args/IntParser.java
+++ src/main/java/org/args/IntParser.java
@@ -5,9 +5,12 @@ import java.util.List;
 class IntParser implements OptionParser {
     @Override
     public Object parse(List<String> arguments, Option option) {
-        // First query the index of the flag ,then get the value and ensure it is an integer
         int index = arguments.indexOf("-" + option.value());
         String value = arguments.get(index + 1);
+        return parseValue(value);
+    }
+
+    private static int parseValue(String value) {
         return Integer.parseInt(value);
     }
 }
