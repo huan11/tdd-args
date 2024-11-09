d93e091fd74d7350c10028bce703292d46a52408
Asher <springflybird@163.com>
2024-11-09 20:05:30 +0800

    [Refactor] Bad Smell:IntParser 和 StringParser 代码有大量的重复
    Step 16: 移除掉没有使用的构造函数 StringParser 并测试

--- src/main/java/org/args/StringParser.java
+++ src/main/java/org/args/StringParser.java
@@ -1,10 +1,6 @@
 package org.args;
 
 class StringParser extends IntParser {
-    private StringParser() {
-        super(String::valueOf);
-    }
-
     public static OptionParser createStringParser() {
         return new IntParser(String::valueOf);
     }
