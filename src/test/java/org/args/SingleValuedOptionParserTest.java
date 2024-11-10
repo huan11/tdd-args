d64f69d5f673fbf97efb7bd343e2e1c839372d5c
Asher <springflybird@163.com>
2024-11-10 15:20:57 +0800

    [SingValueOptionParser-Sad][red]:
    should_not_accept_extra_argument_for_single_valued_option

--- src/test/java/org/args/SingleValuedOptionParserTest.java
+++ src/test/java/org/args/SingleValuedOptionParserTest.java
@@ -1,4 +1,22 @@
 package org.args;
 
+import org.junit.jupiter.api.Test;
+
+import static java.util.Arrays.asList;
+import static org.args.BooleanOptionParserTest.option;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class SingleValuedOptionParserTest {
+    @Test
+    public void should_not_accept_extra_argument_for_single_valued_option() {
+        // Assert
+        TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () -> {
+            // Act
+            new SingValueOptionParser<>(Integer::parseInt).parse(asList("-p", "8080", "8081"), option("p"));
+        });
+
+        // Assert
+        assertEquals("p", e.getOption());
+    }
 }
