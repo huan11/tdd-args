bd4b053f1027e07ed1f8aadcc11338e99fdbfec3
Asher <springflybird@163.com>
2024-11-10 12:31:30 +0800

    [BooleanOptionParser-Sad-more than one arguments]:这个测试其实是我们对需求对理解没有很清楚，我们可以删掉这个测试

--- src/test/java/org/args/BooleanOptionParserTest.java
+++ src/test/java/org/args/BooleanOptionParserTest.java
@@ -22,19 +22,6 @@ public class BooleanOptionParserTest {
         assertEquals("l", e.getOption());
     }
 
-    @Test
-    public void should_not_accept_extra_arguments_for_boolean_option() {
-        // Assert
-        TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () -> {
-            // Act
-            new BooleanOptionParser().parse(asList("-l", "t", "f"), option("l"));
-        });
-
-        // Assert
-        assertEquals("l", e.getOption());
-    }
-
-
     static Option option(String value) {
         return new Option() {
             @Override
