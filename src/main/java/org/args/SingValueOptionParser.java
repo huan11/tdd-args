feb32c72eb149aefd862a3eb8452fe57871a75ce
Asher <springflybird@163.com>
2024-11-10 19:41:11 +0800

    [SingValueOptionParser][Refactor]
    Bad Smell: 代码意图不是很直观
    
    重构思路：变换实现方式。

--- src/main/java/org/args/SingValueOptionParser.java
+++ src/main/java/org/args/SingValueOptionParser.java
@@ -2,6 +2,7 @@ package org.args;
 
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 class SingValueOptionParser<T> implements OptionParser<T> {
     Function<String, T> valueParser;
@@ -12,13 +13,21 @@ class SingValueOptionParser<T> implements OptionParser<T> {
         this.valueParser = valueParser;
     }
 
+
     @Override
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
         if (index == -1) return defaultValue;
-        if (index + 1 == arguments.size() || arguments.get(index + 1).startsWith("-"))
+
+        int followingFlagIndex = IntStream.range(index + 1, arguments.size())
+                .filter(it -> arguments.get(it).startsWith("-"))
+                .findFirst().orElse(arguments.size());
+
+        List<String> values = arguments.subList(index + 1, followingFlagIndex);
+
+        if (values.size() < 1)
             throw new InsufficientArgumentsException(option.value());
-        if (index + 2 < arguments.size() && !arguments.get(index + 2).startsWith("-"))
+        if (values.size() > 1)
             throw new TooManyArgumentsException(option.value());
         return valueParser.apply(arguments.get(index + 1));
     }
