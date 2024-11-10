11af8ef80944dbb3ac2b67ef3f7a51103b5ba0f4
Asher <springflybird@163.com>
2024-11-10 11:22:37 +0800

    [BooleanOptionParser-Sad-extra_argument]:[Green]
    implement should_not_accept_extra_argument_for_boolean_option
    
    use index to implement，and will raise
    one issue for ArgsTest(ArrayIndexOutOfBoundsException)

--- src/main/java/org/args/BooleanOptionParser.java
+++ src/main/java/org/args/BooleanOptionParser.java
@@ -6,6 +6,8 @@ class BooleanOptionParser implements OptionParser {
     @Override
     public Object parse(List<String> arguments, Option option) {
         // If the flag is present, the value is true, otherwise false
-        return arguments.contains("-" + option.value());
+        int index = arguments.indexOf("-" + option.value());
+        if (!arguments.get(index + 1).startsWith("-")) throw new TooManyArgumentsException(option.value());
+        return index != -1;
     }
 }
