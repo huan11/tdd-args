740d9eb6431c8816a5d8e65c1b12f1dd393059c0
Asher <springflybird@163.com>
2024-11-10 20:50:06 +0800

    [BadSmell 重复结构 Refactor]
    9 在 BooleanOptionParser 中 使用 static import 语句导入 SingValueOptionParser

--- src/main/java/org/args/BooleanOptionParser.java
+++ src/main/java/org/args/BooleanOptionParser.java
@@ -2,10 +2,12 @@ package org.args;
 
 import java.util.List;
 
+import static org.args.SingValueOptionParser.values;
+
 class BooleanOptionParser implements OptionParser<Boolean> {
     @Override
     public Boolean parse(List<String> arguments, Option option) {
-        return SingValueOptionParser.values(arguments, option, 0)
+        return values(arguments, option, 0)
                 .map(it -> true).orElse(false);
     }
 }
