7a3ac0a39257ca3cb8b0698c6e739ffc976fc0f5
Asher <springflybird@163.com>
2024-11-10 21:38:58 +0800

    [代码风格不一致]：
    统一 SingValueOptionParser 和 bool 两种实现方式。
    
    inline 从继承方法里面提取出来的方法 getT （清理手术现场）

--- src/main/java/org/args/SingValueOptionParser.java
+++ src/main/java/org/args/SingValueOptionParser.java
@@ -23,17 +23,13 @@ class SingValueOptionParser<T> implements OptionParser<T> {
     }
 
     public static <T> OptionParser<T> createSingValueOptionParser(T defaultValue, Function<String, T> valueParser) {
-        return ((arguments, option) -> getT(arguments, option, valueParser, defaultValue));
+        return ((arguments, option) -> values(arguments, option, 1).map(it -> parseValue(it.get(0), valueParser)).orElse(defaultValue));
     }
 
 
     @Override
     public T parse(List<String> arguments, Option option) {
 
-        return getT(arguments, option, valueParser, defaultValue);
-    }
-
-    private static <T> T getT(List<String> arguments, Option option, Function<String, T> valueParser, T defaultValue) {
         return values(arguments, option, 1).map(it -> parseValue(it.get(0), valueParser)).orElse(defaultValue);
     }
 
