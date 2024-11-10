e47ffe43c0a6c52fa0eeea6656032b8b884e1398
Asher <springflybird@163.com>
2024-11-10 16:02:21 +0800

    [SingValueOptionParser-Sad-2][implement-red]: should_not_accept_insufficient_argument_for_single_valued_option
    当前实现的时候考虑的是  -p -l 的参数不足的情况，但是实际测的是单独 -p 的情况，两种都属于同一种测试，就是测试数据不同。

--- src/main/java/org/args/SingValueOptionParser.java
+++ src/main/java/org/args/SingValueOptionParser.java
@@ -13,6 +13,8 @@ class SingValueOptionParser<T> implements OptionParser<T> {
     @Override
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
+        if (arguments.get(index + 1).startsWith("-"))
+            throw new InsufficientArgumentsException(option.value());
         if (index + 2 < arguments.size() &&
                 !arguments.get(index + 2).startsWith("-")) throw new TooManyArgumentsException(option.value());
 
